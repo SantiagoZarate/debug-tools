45e60249ceaecf0141a8be5801e8078bbd56f5c5
Santiago Zarate <santiago2001zarate@gmail.com>
2024-02-16 02:19:36 -0300

    fix: add debug tool data

---
+++ src/data/debugtools.ts
@@ -0,0 +1,71 @@
+export default {
+  tools: [
+    {
+      name: "lighthouse",
+      category: "extension",
+      description: "...",
+      picture: "...",
+      url: "https://page-speed.dev/"
+    },
+    {
+      name: "axe",
+      category: "webpage",
+      description: "...",
+      picture: "...",
+      url: "https://page-speed.dev/"
+    },
+    {
+      name: "css overview",
+      category: "extension",
+      description: "...",
+      picture: "...",
+      url: "https://page-speed.dev/"
+    },
+    {
+      name: "squoosh",
+      category: "extension",
+      description: "...",
+      picture: "...",
+      url: "https://page-speed.dev/"
+    },
+    {
+      name: "web page test",
+      category: "webpage",
+      description: "...",
+      picture: "...",
+      url: "https://page-speed.dev/"
+    },
+    {
+      name: "page speed",
+      category: "webpage",
+      description: "...",
+      picture: "...",
+      url: "https://page-speed.dev/"
+    },
+    {
+      name: "wappalyzer",
+      category: "extension",
+      description: "...",
+      picture: "...",
+      url: "https://page-speed.dev/"
+    },
+    {
+      name: "open graph",
+      category: "webpage",
+      description: "...",
+      picture: "...",
+      url: "https://page-speed.dev/"
+    },
+
+  ]
+} satisfies { tools: DebugTool[] }
+
+interface DebugTool {
+  name: string,
+  url: string,
+  picture: string,
+  description: string,
+  category: DebugToolCategory
+}
+
+type DebugToolCategory = 'extension' | 'webpage'
