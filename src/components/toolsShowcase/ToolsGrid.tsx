998e99acefd89fae0d23853896195f558fcdd91a
Santiago Zarate <santiago2001zarate@gmail.com>
2024-02-17 04:03:44 -0300

    style: change grid tools styles

--- src/components/toolsShowcase/ToolsGrid.tsx
+++ src/components/toolsShowcase/ToolsGrid.tsx
@@ -6,25 +6,24 @@ type Props = {
 
 export function ToolsGrid({ tools }: Props) {
   return (
-    <ul className="grid grid-cols-responsive gap-8 ">
+    <ul className="grid grid-cols-responsive gap-8">
       {tools.map(tool => (
         <a
           href={tool.url}
           target='_blank'
-          className='flex flex-col rounded-lg hover:scale-[103%] hover:outline hover:shadow-xl duration-300 transition-all'
+          className='flex items-center rounded-lg hover:scale-[103%] hover:outline bg-stone-900/60 border border-white/30 hover:shadow-xl duration-300 transition-all'
           key={tool.name} rel="noreferrer">
-          <figure className='p-6 bg-gradient-to-tr from-zinc-950 from-30% to-violet-900'>
+          <figure className='p-6 min-w-fit'>
             <img
-              className='mx-auto rounded-full outline bg-stone-200 object-cover w-[100px] h-[100px]'
+              className='rounded-full outline bg-stone-200 object-cover w-[100px] h-[100px]'
               src={tool.pictures.logo} alt={`${tool.name} picture`}
               title={tool.name} />
           </figure>
           <article className='flex flex-col gap-2 p-2'>
-            <header className='flex gap-2 items-center'>
-              <h3 className='capitalize text-xl font-bold'>{tool.name}</h3>
-              <span className='text-xs px-3 py-1 bg-stone-900 rounded-full'>{tool.category}</span>
-            </header>
-            <p className='text-xs tracking-wide text-stone-400'>{tool.description}</p>
+            <h3 className='capitalize text-xl font-bold'>{tool.name}</h3>
+            <span className='text-xs px-3 py-1 bg-stone-800 rounded-full w-fit'>{tool.category}</span>
+            {/* <header className='flex gap-2 items-center'></header> */}
+            <p className='text-xs tracking-wide text-stone-400 text-ellipsis'>{tool.description}</p>
           </article>
         </a>
       ))}
